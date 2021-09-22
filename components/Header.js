38c86a2f0419f9903fbd67f87507d46bbede0527
Madstick <ziliangz@gmail.com>
2021-09-22 14:31:14 -0700

    first commit

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="p-4">
+      <h1 className="font-inter text-yellow-300 text-7xl leading-normal">My Blog</h1>
+      <p className="font-inter text-gray-400 text-xl">Darkmode is default not alternative.</p>
+    </div>
+  );
+}
---
+++ posts/test.md
@@ -0,0 +1,10 @@
+---
+title: 'Test'
+date: 'June 24 2021'
+---
+
+## Heading 2
+* Item 1
+* Item 2
+
+This is a sample test
