1c6f7de96833aca391d3820af02e22c9a859ca3a
Madstick <ziliangz@gmail.com>
2021-10-22 00:19:04 -0700

    add first blog post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import marked from "marked"; 
+import marked from "marked";
 import Link from "next/link";
 
 export default function PostPage({
@@ -15,15 +15,13 @@ export default function PostPage({
         <a className="font-inter bg-yellow-400 p-2">{"<< "}Go Back</a>
       </Link>
       <div className="post-page">
-        <h1 className="post-title text-gray-700 text-5xl pt-8 pb-8">
-          {title}
-        </h1>
+        <h1 className="post-title text-gray-700 text-5xl pt-8 pb-8">{title}</h1>
         <div className="post-date">{date}</div>
         <div className="post-body">
-          <div 
-          className="prose"
-          dangerouslySetInnerHTML={{ __html: marked
-          (content) }}></div>
+          <div
+            className="prose"
+            dangerouslySetInnerHTML={{ __html: marked(content) }}
+          ></div>
         </div>
       </div>
     </div>
@@ -47,8 +45,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params: { slug } }) {
   const markdownWithMeta = fs.readFileSync(
-    path.join('posts', slug + '.md'),
-    'utf-8'
+    path.join("posts", slug + ".md"),
+    "utf-8"
   );
 
   const { data: frontmatter, content } = matter(markdownWithMeta);
---
+++ posts/firstPost.md
@@ -0,0 +1,14 @@
+---
+title: 'First post'
+date: 'Oct 21 2021'
+---
+
+# Hello, world! (Like, seriously, HELLO!)
+
+After working on this website on-and-off for a month, I was finally able to push it online. 
+
+This is my first hand-coded personal website. I'm planning to add more and more features in future. 
+
+The site looks pretty simple right now. It only has some minimal styling with a markdown blog.
+
+As I just entered the world of development, I hope to share all my joy and sweats in my coding journry. I am a self-taught developer. There are lots of learnings I would like to share with other developers.
